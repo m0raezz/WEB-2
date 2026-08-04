041bd70c85db7ee1bde71f6e49a3ea7ae64b7799
Renan Brito de Moraes <71576605+m0raezz@users.noreply.github.com>
2026-08-03 21:58:53 -0300

    Delete AULA 1 - 27_07_26 directory

--- AULA 1 - 27_07_26/inteira.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $valor = 53.90;
-    
-    echo "<p style='color: purple; font-size: 5em; text-decoration: underline overline black; text-align: center; margin: 2em;'>
-        Inteira: $valor  </p>" ;
-?>
--- AULA 1 - 27_07_26/meia.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $valor = 53.90/2;
-    
-    echo "<p style='color: purple; font-size: 5em; text-decoration: underline overline black; text-align: center; margin: 2em;'>
-        Meia: $valor </p>" ;
-?>
